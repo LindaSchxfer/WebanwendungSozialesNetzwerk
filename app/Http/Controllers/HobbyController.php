40b65dbae0f3d2e251a43eccf06b968d48a6f8b5
LindaSchxfer <lindaschaefer98@gmail.com>
2021-11-15 18:45:47 +0100

    Hobby löschen

--- app/Http/Controllers/HobbyController.php
+++ app/Http/Controllers/HobbyController.php
@@ -111,6 +111,10 @@ class HobbyController extends Controller
      */
     public function destroy(Hobby $hobby)
     {
-        //
+        $old_name = $hobby->name;
+        $hobby->delete();
+        return $this->index()->with([
+            'meldung_success' => 'Dein Hobby <b>' . $old_name . '</b> wurde erfolgreich gelöscht.'
+        ]);
     }
 }
