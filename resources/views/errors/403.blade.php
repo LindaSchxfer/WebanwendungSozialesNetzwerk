ab646969e421e2e6308f8349fb0f1403b2a1a3ef
LindaSchxfer <lindaschaefer98@gmail.com>
2021-11-24 14:34:45 +0100

    Individuelle lustige Error meldungen

---
+++ resources/views/errors/403.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>403</title>
+</head>
+<body style="background: url('/error-image-lindagram.jpg'); background-size: cover; background-repeat: no-repeat; background-position: center">
+    <div style="color:white; margin:100px auto; text-align:center; ">
+        <h1 style="font-size:240%; margin-top:40%;">403 - Ups, da ist was schief gelaufen!</h1>
+    </div> 
+</body>
+</html>
